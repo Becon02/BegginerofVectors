aaa8863c56b35bbbca8ea4734a30a5df47ca84f3
Becon02 <martinuco569@gmail.com>
2023-01-31 15:40:02 +0000

    Finish Workshop
    -Finished following script with conditions.
    -Added a trail renderer that saws the radius
    -Added raycast to know directions

--- Assets/Scripts/EvaderMovement.cs
+++ Assets/Scripts/EvaderMovement.cs
@@ -6,15 +6,42 @@ public class EvaderMovement : MonoBehaviour
 {
     [Header("Settings")]
     [SerializeField] private float moveSpeed;
+    public MyVector3 movement = new MyVector3(0,0,0);
+    public MyVector3 movNormalize = new MyVector3(0,0,0);
+    private MyVector3 position;
+
 
     // Update is called once per frame
     void Update()
     {
+        //Gets the input of the player
         float inputX = Input.GetAxisRaw("Horizontal");
         float inputY = Input.GetAxisRaw("Vertical");
 
-        MyVector3 movement = new MyVector3(inputX * moveSpeed, inputY * moveSpeed, 0);
-        
-        transform.position += movement.ToUnityVector() * Time.deltaTime;
+        //Gets for the position of the sphere in game
+        position = new MyVector3(transform.position.x, transform.position.y, transform.position.z);
+
+        //Checks if theres input, if not it doesnt move
+        if(inputX != 0 || inputY != 0)
+        {
+            //Gets the direction it should move
+            movement =  new MyVector3(inputX, inputY, 0);
+
+            //Normalizes the direction of movement
+            movNormalize = movement.NormalizeVector();
+
+            //Multiplies the direction with a speed variable to move at a certain speed
+            MyVector3 moveVelocity = MyVector3.ScaleVector(movement, moveSpeed);
+
+            //Updates the position of the sphere in a velocity
+            transform.position += moveVelocity.ToUnityVector() * Time.deltaTime;
+        }
+
+        Raycasting();
+    }
+
+    private void Raycasting()
+    {
+        Debug.DrawRay(position.ToUnityVector(), movNormalize.ToUnityVector() * 2, Color.green);
     }
 }
--- Assets/Scripts/FindObject.cs
+++ Assets/Scripts/FindObject.cs
@@ -11,30 +11,45 @@ public class FindObject : MonoBehaviour
 
     [Header("Settings")]
     [SerializeField] private float moveSpeed;
-
-    private void Start()
-    {
-        oldPos = new MyVector3(transform.position.x, transform.position.y, transform.position.z);
-    }
+    [SerializeField] private EvaderMovement evaderMovement_script;
 
     private void Update()
     {
+        //Position of the cube
+        oldPos = new MyVector3(transform.position.x, transform.position.y, transform.position.z);
+
+        //Position of the sphere
         finalPos = new MyVector3(sphere.transform.position.x, sphere.transform.position.y, sphere.transform.position.z);
 
-        if(transform.position != finalPos.ToUnityVector())
+        
+
+
+        //Check if the objects are not in the same place
+        if (transform.position != finalPos.ToUnityVector())
         {
+            //Gets the direction it should move
             MyVector3 direction = MyVector3.SubtractVector(finalPos, oldPos);
 
+            //Calculates the distance between the cube and the sphere
+            float distance = direction.Length();
+
+            //Normalizes the direction vector
             MyVector3 normDirection = direction.NormalizeVector();
+
+            //Multiplies the direction with a speed variable to move at a certain speed
             MyVector3 moveVelocity = MyVector3.ScaleVector(normDirection, moveSpeed);
 
-            transform.position += moveVelocity.ToUnityVector() * Time.fixedDeltaTime;
-        }
-    }
+            //Dot product of the positions
+            float dotProductVectors = MyVector3.DotProductVector(direction, evaderMovement_script.movement, false);
 
-    private void FixedUpdate()
-    {
-        //Raycasting();
+            //Checks that the cube is behind the sphere and if its close enough in order to move towards it
+            if(dotProductVectors >= 0.5 && distance < 15)
+            {
+                transform.position += moveVelocity.ToUnityVector() * Time.fixedDeltaTime;
+            }
+        }
+        
+        Raycasting();
     }
 
     private void Raycasting()
